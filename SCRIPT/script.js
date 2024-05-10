2c84572d0360f66858798b50af0addd7ec84fab8
Edwarddev0723 <renhehuang0723@gmail.com>
2024-05-11 00:25:08 +0800

    首頁初版

---
+++ SCRIPT/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.testimonial-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
+    });
+});
