11175562279c858144b0a32491ab02bf8897f48d
Edwarddev0723 <renhehuang0723@gmail.com>
2024-05-11 23:03:43 +0800

    增加搜尋頁面

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -30,3 +30,29 @@ $(document).ready(function() {
         }
     });
 });
+
+$(document).ready(function() {
+    $('.farm-slider').slick({
+        autoplay: true, // 啟用自動播放
+        autoplaySpeed: 3000, // 每3秒切換一次
+        infinite: true, // 無限循環播放
+        slidesToShow: 3, // 一次顯示3個農場
+        slidesToScroll: 1, // 一次滑動1個農場
+        arrows: false,
+        dots: false, // 不顯示導航點
+        responsive: [
+            {
+                breakpoint: 768, // 當視窗寬度小於768px時
+                settings: {
+                    slidesToShow: 2 // 一次顯示2個農場
+                }
+            },
+            {
+                breakpoint: 480, // 當視窗寬度小於480px時
+                settings: {
+                    slidesToShow: 1 // 一次顯示1個農場
+                }
+            }
+        ]
+    });
+});
