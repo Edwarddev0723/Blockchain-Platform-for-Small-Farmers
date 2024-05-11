58ad5b7f3290ab04b8b0675859ddfafeb568b875
Edwarddev0723 <renhehuang0723@gmail.com>
2024-05-11 13:27:27 +0800

    增加登入彈出視窗

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -11,3 +11,22 @@ $(document).ready(function() {
         nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
     });
 });
+
+$(document).ready(function() {
+    // 點擊登入按鈕時顯示彈出視窗
+    $(".login-btn").click(function() {
+        $("#login-modal").fadeIn();
+    });
+
+    // 點擊關閉按鈕時隱藏彈出視窗
+    $(".close").click(function() {
+        $("#login-modal").fadeOut();
+    });
+
+    // 點擊彈出視窗外部時隱藏彈出視窗
+    $(window).click(function(event) {
+        if (event.target == $("#login-modal")[0]) {
+            $("#login-modal").fadeOut();
+        }
+    });
+});
