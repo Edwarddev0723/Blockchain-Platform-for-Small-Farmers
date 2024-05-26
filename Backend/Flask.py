fa587800225ac35e967f768268d782d2436f7c6c
Edwarddev0723 <renhehuang0723@gmail.com>
2024-05-26 14:30:54 +0800

    後端api

---
+++ Backend/Flask.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request, Response
+from flask_cors import CORS
+import json
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+UPLOAD_FOLDER = 'uploads'
+app.config['uploads'] = UPLOAD_FOLDER
+
+# 讀取JSON檔案
+def load_products_from_json():
+    with open('products.json', 'r', encoding='utf-8') as file:
+        products_data = json.load(file)
+    return products_data
+
+# 返回所有產品信息
+@app.route('/api/products', methods=['GET'])
+def get_products():
+    products = load_products_from_json()
+    return jsonify(products)
+
+# 創建新產品
+@app.route('/api/products/create', methods=['POST'])
+def create_product():
+    data = request.json
+    image = request.files['image']  # 取得用戶上傳的照片文件
+    # 將照片保存在指定的上傳資料夾內
+    image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
+    products = load_products_from_json()
+    products.append(data)
+    save_to_json(products)  # 將更新後的數據寫入JSON檔案
+    return Response(status=201)
+
+# 將數據寫入JSON檔案
+def save_to_json(data):
+    with open('products.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+if __name__ == '__main__':
+    app.run(debug=True)
